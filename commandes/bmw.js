9462d32d2e76a3f00885fbedb34bd2f544cdb8b9
gidiowis <160538903+gidiowis@users.noreply.github.com>
2024-07-07 03:35:15 +0300

    Update bmw.js

--- commandes/bmw.js
+++ commandes/bmw.js
@@ -20,8 +20,8 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *MBUVI-MD-REPO* \n\n '+'je suis un bot Whatsapp Multi-appareil '
-      let d =' developpé par *MBUVI-MD-REPO*'
+   let z ='Salut je m\'appelle *Tennor-MD-REPO* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *Tennor-MD-REPO*'
       let varmess=z+d
       var img='https://telegra.ph/file/a7ca8d4777fa8ad977795.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
