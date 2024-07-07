ab5df0ff33ec54881345fc5c23349be5858bb737
gidiowis <160538903+gidiowis@users.noreply.github.com>
2024-07-07 14:50:27 +0300

    Update Test.js

--- commandes/Test.js
+++ commandes/Test.js
@@ -4,7 +4,7 @@ const { france } = require("../framework/france");
 france({ nomCom: "test", reaction: "⏳", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = '𝘽𝙊𝙏 𝙄𝙎 𝙊𝙉𝙇𝙄𝙉𝙀 \n\n ' + "║❒ 𝙏𝙔𝙋𝙀 𝙈𝙀𝙉𝙐 𝙏𝙊 𝙎𝙀𝙀 𝘾𝙊𝙈𝙈𝘼𝙉𝘿𝙎";
-    let d = '                                            ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://www.youtube.com/Rhodvick';
+    let d = '                                            ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://www.youtube.com/gideonowino';
     let varmess = z + d;
     var img = 'https://telegra.ph/file/a7ca8d4777fa8ad977795.jpg';
     await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
@@ -19,8 +19,8 @@ console.log("mon test");
 
 async function  execute  (origineMessage,zok) {
   console.log("Commande saisie !!!s")
-   let z ='Salut je m\'appelle *MBUVI-MD-REPO* \n\n '+'je suis un bot Whatsapp Multi-appareil '
-      let d =' developpé par *MBUVI-MD-REPO*'
+   let z ='Salut je m\'appelle *Tennor-MD-REPO* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *Tennor-MD-REPO*'
       let varmess=z+d
       var img='https://telegra.ph/file/a7ca8d4777fa8ad977795.jpg'
 await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
